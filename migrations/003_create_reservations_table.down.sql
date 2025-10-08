842385f4e79f8f598b422e4b7ef5c85d3bf1962a
MatiasTelo <matias.telo000@gmail.com>
2025-10-08 15:51:19 -0300

    actualizar README.md

--- migrations/003_create_reservations_table.down.sql
+++
@@ -1,7 +0,0 @@
--- Drop stock_reservations table
-DROP INDEX IF EXISTS idx_reservations_active;
-DROP INDEX IF EXISTS idx_reservations_expires_at;
-DROP INDEX IF EXISTS idx_reservations_status;
-DROP INDEX IF EXISTS idx_reservations_order_id;
-DROP INDEX IF EXISTS idx_reservations_article_id;
-DROP TABLE IF EXISTS stock_reservations;
--- migrations/004_drop_reservations_table.up.sql
+++
@@ -1,7 +0,0 @@
--- Drop stock_reservations table - simplification to event-driven approach
-DROP INDEX IF EXISTS idx_reservations_active;
-DROP INDEX IF EXISTS idx_reservations_expires_at;
-DROP INDEX IF EXISTS idx_reservations_status;
-DROP INDEX IF EXISTS idx_reservations_order_id;
-DROP INDEX IF EXISTS idx_reservations_article_id;
-DROP TABLE IF EXISTS stock_reservations;
